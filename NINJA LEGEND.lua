a7852b6de33fb7af82cc7961497f2de60dbdd766
hajibeza <74174244+hajibeza@users.noreply.github.com>
2021-08-04 13:15:25 +0700

    Update NINJA LEGEND.lua

--- NINJA LEGEND.lua
+++ NINJA LEGEND.lua
@@ -1,4 +1,3 @@
-
 local DiscordLib = loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/discord%20lib.txt")()
 
 local win = DiscordLib:Window("RIPPER HUB | Ninja legend")
